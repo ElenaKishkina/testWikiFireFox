daf1f3fb7ac03953f572588d5d553f81b0fc7636
Elena <elena@kishkin.su>
2019-09-19 16:10:51 +0300

    firefox test

---
+++ src/test/java/testWikiFireFox.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testWikiFireFox {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        //WebDriverManager.firefoxdriver().version("0.19.1").setup();
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeMethod
+    public void setupTest() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://en.wikipedia.org");
+        Thread.sleep(3000);
+
+        WebElement webElement = driver.findElement(By.xpath("//a[@href='/wiki/Wikipedia:About']"));
+        webElement.click();
+        Thread.sleep(3000);
+        WebElement logo = driver.findElement(By.xpath("//h1[@id='firstHeading']"));
+        Assert.assertTrue(logo.isDisplayed());
+
+    }
+}
